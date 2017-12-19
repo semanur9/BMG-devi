3e242522d17b81e178fd74eb0ba3c720aa4b2c1c
semanur9 <34682032+semanur9@users.noreply.github.com>
2017-12-19 17:11:46 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad;
+            Console.Write("ADINIZI GİRİNİZ:");
+            ad = Console.ReadLine();
+            foreach(char a in ad)
+                Console.Write((char)(a+10));
+            Console.ReadKey();
+        }
+    }
+}
